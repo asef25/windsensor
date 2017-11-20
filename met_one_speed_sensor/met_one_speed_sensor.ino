2b4903a0df4b9bfbbe16687bd511834e3734bd9a
Thomas D Turner <thomastdt@gmail.com>
2017-11-20 13:37:40 -0800

    timer1 working
    timer1 is set to interrupt every 4 seconds.
    
    rpm - revs per minute is calculated
    mph - miles per hour is calculated

--- met_one_speed_sensor/met_one_speed_sensor.ino
+++ met_one_speed_sensor/met_one_speed_sensor.ino
@@ -1,28 +1,60 @@
-const byte interruptPin = 3;
-volatile int counter;
-volatile int rev;
+const byte interruptPin = 2;
+volatile int counter; //# of pulses
+volatile long rpm;    //revs per min
+volatile bool rw_flag;
+volatile float V;     //Velocity [miles per hour]
 
-void irq_handler(){
-    ++counter;
-    if(counter == 40){
-        ++rev;
-        counter = 0;
-    }
-}
-
-void setup() {
+void setup()
+{
+  
     while (!Serial) {
         ; // wait for serial port to connect. Needed for native USB port only
     }
     Serial.begin(57600);
-    pinMode(interruptPin, INPUT);
-    attachInterrupt(digitalPinToInterrupt(interruptPin), irq_handler, RISING );
-    counter = rev = 0;
+    
+    // initialize timer1 - 16 bit 65536
+    noInterrupts();           // disable all interrupts
+    TCCR1A  = 0;
+    TCCR1B  = 0;
+    
+    TCNT1   = 15625;            // preload timer 16MHz/1024 => Period=4sec
+    TCCR1B |= ((1 << CS12)| (1 << CS10)) ;    // 1024 prescaler 
+    TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
+    interrupts();             // enable all interrupts
+
+    // initialize timer0 - rising edge triggered interrupt - pin 2
+    attachInterrupt(digitalPinToInterrupt(interruptPin), pin_irq_handler, RISING );
+
+    // initialize variables
+    counter = 0;
+    rpm     = 0;
+    rw_flag = 0;
+    V       = 0.0f;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    Serial.println(rev);
+ISR(TIMER1_OVF_vect)        
+{
+    rpm     = counter * 15L;
+    rpm    /= 40L;
+    V       = (rpm / 16.767f) + 0.6f;
+    rw_flag = 1;
+    counter = 0;
+}
+
+void pin_irq_handler()
+{
+    ++counter;
+}
+
+void loop() 
+{
+    if(rw_flag){
+
+        Serial.print(rpm);
+        Serial.print(" ");
+        Serial.println(V);
+        rw_flag = !rw_flag;
+    }
 }
 
 
