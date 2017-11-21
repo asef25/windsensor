ba930dc58ff77de8d5c4b19560282c2aa438f3ab
Thomas D Turner <thomastdt@gmail.com>
2017-11-21 13:39:51 -0800

    remove rtc initialization settings

--- windsensor.ino
+++ windsensor.ino
@@ -42,8 +42,7 @@ void setup(void)
         // This line sets the RTC with an explicit date & time, for example to set
         // January 21, 2014 at 3am you would call:
         // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-    }else
-        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
       
     
     Serial.print("\nInitializing SD card...");
